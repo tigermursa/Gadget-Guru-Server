5f059cb3dd83ae9131563dd037fa39b29af60b49
Mursalin Hossain <mursalin2311@gmail.com>
2024-03-23 04:58:52 +0600

    "Product interface created"

--- src/app/modules/phones/phone.interface.ts
+++ src/app/modules/phones/phone.interface.ts
@@ -1,12 +1,10 @@
 import { Model } from "mongoose";
 
-
-
-export interface PhoneWithStatic extends Model<Phone> {
-    isUserExists(id: string): Promise<Phone | null>;
+export interface ProductWithStatic extends Model<Product> {
+    isUserExists(id: string): Promise<Product | null>;
 }
 
-export type Phone = {
+export type Product = {
     image: string;
     name: string;
     brand: string;
@@ -18,27 +16,7 @@ export type Phone = {
     description: string;
     review: number;
     rating: number;
-    priceV1: string;
-    ramV1: string;
-    priceV2: string;
-    ramV2: string;
     tag: string;
-    fullSpec: {
-        operatingSystem: string;
-        processor: string;
-        display: string;
-        cameras: {
-            main: string;
-            ultraWide: string;
-            macro: string;
-            depth: string;
-        };
-        battery: string;
-        charging: string;
-        dimensions: string;
-        weight: string;
-        colors: string[];
-    };
     isDeleted?: boolean;
 };
 
